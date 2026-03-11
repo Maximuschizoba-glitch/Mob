524ea452e6bb56763f194eec38e7a962177e063a
Maximuschizoba-glitch <Maximuschizoba@icloud.com>
2026-03-11 23:52:39 +0100

    Initial Commit

---
+++ Mob/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mob
+//
+//  Created by Maximus Chizoba on 11/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mob/MobApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobApp.swift
+//  Mob
+//
+//  Created by Maximus Chizoba on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
