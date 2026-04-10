75f693e68f9c26bf6784a37f0f9ae5f30aa0e1b1
Maximuschizoba-glitch <Maximuschizoba@icloud.com>
2026-04-10 03:40:34 +0100

    Security: default DEV_MODE to false (disable OTP bypass in production)

--- mob_app/lib/core/constants/app_config.dart
+++ mob_app/lib/core/constants/app_config.dart
@@ -1,33 +1,23 @@
-
-
 class AppConfig {
   AppConfig._();
 
-
   static const bool isDevMode = bool.fromEnvironment(
     'DEV_MODE',
-    defaultValue: true,
+    defaultValue: false,
   );
-
 
   static const bool otpBypassEnabled = isDevMode;
 
-
   static const String devOtpCode = '123456';
-
 
   static const String webBaseUrl = 'https://mob.getbuukride.com';
 
-
   static String get termsUrl => '$webBaseUrl/terms';
-
 
   static String get privacyUrl => '$webBaseUrl/privacy';
 
-
   static String happeningShareUrl(String uuid) =>
       '$webBaseUrl/happenings/$uuid';
-
 
   static String ticketShareUrl(String uuid) => '$webBaseUrl/tickets/$uuid';
 }
